6017875ef70224db7d39d1509fcab9166302ae8c
Elena Mogilevich <nervuse@gmail.com>
2022-07-07 12:13:19 +0200

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by e.mogilevich on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
